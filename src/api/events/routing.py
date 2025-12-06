9688db2068de8397bd2f923e8b7817ea26d5817e
rarba17 <mmaapril42@gmail.com>
2025-12-07 01:12:41 +0530

    done with routing need to setup timescaledb

--- src/api/events/routing.py
+++ src/api/events/routing.py
@@ -1,11 +1,28 @@
 from fastapi import APIRouter
-from .schemas import EventSchems
+from .schemas import EventSchemas,EventListSchems,EventCreateSchemas,EventUpdateSchemas
 router = APIRouter()
 
 @router.get("/")
-def read_items():
-  return {"items":[1,2,3,4]}
+def read_events() -> EventListSchems:
+  return {
+    "result":[{"id": 1},{"id": 2},{"id": 3},{"id": 4}] , "count":1
+
+          }
+
+@router.post("/")
+def create_event(payload:EventCreateSchemas) -> EventSchemas:
+  print(payload.page)
+  data = payload.model_dump()
+  return {"id": 123, **data}
 
 @router.get("/{event_id}")
-def get_items(event_id:int) -> EventSchems:
-  return {"id":event_id}
+def get_event(event_id:int) -> EventSchemas:
+  return{"id":event_id}
+
+@router.put("/{event_id}")
+def update_event(event_id:int,payload:EventUpdateSchemas) -> EventSchemas:
+  print(payload.description)
+  data = payload.model_dump()
+  return {"id": event_id, **data}
+
+
--- src/api/events/schemas.py
+++ src/api/events/schemas.py
@@ -1,5 +1,18 @@
 from pydantic import BaseModel
+from typing import List, Optional
 
-class EventSchems(BaseModel): # {"id": 12}
+class EventCreateSchemas(BaseModel): # {"id": 12}
+  page: str
+
+class EventUpdateSchemas(BaseModel): # {"id": 12}
+  description: str
+
+class EventSchemas(BaseModel): # {"id": 12}
   id: int
+  page: Optional[str] = ""
+  description: Optional[str] = ""
+
+class EventListSchems(BaseModel): # {"id": 12}
+  result : List[EventSchemas]
+  count : int
 
